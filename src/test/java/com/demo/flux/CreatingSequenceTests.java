b62170bc3495a6671ea312a4e517d77fd16c02e7
Kamal Hossain <kamalbctg@gmail.com>
2020-05-07 16:37:08 +0600

    update on generator

--- src/test/java/com/demo/flux/CreatingSequenceTests.java
+++ src/test/java/com/demo/flux/CreatingSequenceTests.java
@@ -1,9 +1,7 @@
 package com.demo.flux;
 
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.ConnectableFlux;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.SignalType;
+import reactor.core.publisher.*;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
@@ -12,6 +10,8 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 
 class CreatingSequenceTests {
@@ -98,4 +98,49 @@ class CreatingSequenceTests {
                 .expectNext("C")
                 .verifyComplete();
     }
+
+    @Test
+    void streamOfFluxWithCreate() {
+        //Programmatically create a Flux with the capability of emitting multiple elements in a synchronous or
+        // asynchronous manner through the FluxSink API. This includes emitting elements from multiple threads.
+        Flux<Integer> fluxTest = Flux.create((FluxSink<Integer> fluxSink) -> {
+            IntStream.range(0, 5).peek(i -> System.out.println("going to emit - " + i)).forEach(fluxSink::next);
+        });
+
+        fluxTest.delayElements(Duration.ofMillis(1)).subscribe(i -> System.out.println("subscriber-1 :: " + i));
+        fluxTest.delayElements(Duration.ofMillis(2)).subscribe(i -> System.out.println("subscriber-2 :: " + i));
+    }
+
+    @Test
+    void streamOfFluxWithCreateAndOverFlowStrategy() {
+        //Programmatically create a Flux with the capability of emitting multiple elements in a synchronous or
+        // asynchronous manner through the FluxSink API. This includes emitting elements from multiple threads.
+        Flux<Integer> fluxTest = Flux.create((FluxSink<Integer> fluxSink) -> {
+            IntStream.range(0, 5).peek(i -> System.out.println("going to emit - " + i)).forEach(fluxSink::next);
+        },FluxSink.OverflowStrategy.DROP);
+
+        fluxTest.delayElements(Duration.ofMillis(1)).subscribe(i -> System.out.println("subscriber-1 :: " + i));
+        fluxTest.delayElements(Duration.ofMillis(2)).subscribe(i -> System.out.println("subscriber-2 :: " + i));
+    }
+
+     @Test
+    void streamOfFluxWithGenerate() throws InterruptedException {
+         /**
+          * Programmatically create a Flux by generating signals one-by-one via a consumer callback.
+          * generate method is aware of downstream observers processing speed
+          **/
+         AtomicInteger atomicInteger = new AtomicInteger();
+
+         Flux<Integer> integerFlux = Flux.generate((SynchronousSink<Integer> synchronousSink) -> {
+             System.out.println("Flux generate"+ atomicInteger.get());
+             synchronousSink.next(atomicInteger.getAndIncrement());
+         });
+
+         integerFlux.delayElements(Duration.ofMillis(50))
+                 .subscribe(i -> System.out.println("First consumed ::" + i));
+
+         Thread.sleep(Duration.ofSeconds(3).toMillis());
+    }
+
+
 }
